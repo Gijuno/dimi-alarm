b85c87ccf2ba53a89487ec910d6aef3d417a182d
gijuno <itx.ljh.developer@gmail.com>
2021-07-09 15:30:32 +0900

    feat: can play song at fixed time

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import Timer from './Timer'
+import audio from "./dimigo_bell.mp3"
+
+const player = document.createElement('audio')
+
+export const bellPlayer = () => {
+  console.log("ssp?")  
+  player.src = audio
+  player.autoplay = true
+  document.body.append(player)
+
+  console.log(player)
+}
 
 function App() {
   return (
-    <div className="App">
-      <Timer/>
+    <div className="App" onClick={bellPlayer}>
+      <Timer />
     </div>
   );
 }
