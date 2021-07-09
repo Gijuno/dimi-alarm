05d9404d580f977d5e18bddce2fd301810659610
gijuno <itx.ljh.developer@gmail.com>
2021-07-09 15:35:52 +0900

    feat: change interval 1sec to 1min

--- src/Timer.js
+++ src/Timer.js
@@ -25,7 +25,7 @@ class Timer extends React.Component {
                 this.setState({ alarm: checked[0] });
                 this.setState({ minute: this.state.date.getMinutes()});
             },
-            1000
+            60000
         );
     }
 
