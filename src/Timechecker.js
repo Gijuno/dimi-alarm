bf81687fbf0213d5ab6c3c72ff1650e972709e3e
김시윤 <74858992+Im-Siyoun@users.noreply.github.com>
2021-08-09 10:36:46 +0900

    fix: table map to array

--- src/Timechecker.js
+++ src/Timechecker.js
@@ -2,29 +2,34 @@ import {bellPlayer} from "./App.js"
 var count = 0
 const Timechecker = (now) => {
     console.log(now);
-    const times = new Map([
-        ['0808', "1교시 시작"],
-        ['2122', "1교시 끝"],
-        ['1000', "2교시 시작"],
-        ['1050', "2교시 끝"],
-        ['1100', "3교시 시작"],
-        ['1150', "3교시 끝"],
-        ['1200', "4교시 시작"],
-        ['1250', "4교시 끝"],
-        ['1350', "5교시 시작"],
-        ['1440', "5교시 끝"],
-        ['1450', "6교시 시작"],
-        ['1540', "6교시 끝"],
-        ['1550', "7교시 시작"],
-        ['1640', "7교시 끝"]
-    ]);
-    if([...times.keys()].includes(now)) {
+    const times = [
+        ['0900', "1교시 시작", 1],
+        ['0950', "1교시 끝", 2],
+        ['1000', "2교시 시작", 1],
+        ['1050', "2교시 끝", 2],
+        ['1100', "3교시 시작", 1],
+        ['1150', "3교시 끝", 2],
+        ['1200', "4교시 시작", 1],
+        ['1250', "4교시 끝", 3],
+        ['1350', "5교시 시작", 1],
+        ['1440', "5교시 끝", 2],
+        ['1450', "6교시 시작", 1],
+        ['1540', "6교시 끝", 2],
+        ['1550', "7교시 시작", 1],
+        ['1640', "7교시 끝", 4]
+    ];
+    const checker = []
+    for (let i in times) {
+        checker.push(times[i][0].indexOf(now) !== -1 ? i : false)
+    }
+    const index = checker.findIndex((x) => x !== false)
+    if(index !== -1) {
         count++
         if (count===1) {
             console.log("SUCCESS"); 
             console.log("count === " + count);
             bellPlayer()
-            return times.get(now);
+            return [times[index][1], times[index][2]];
         } else {
             console.log("count === " + count);
             return "수업중";
--- src/Timer.js
+++ src/Timer.js
@@ -14,20 +14,6 @@ class Timer extends React.Component {
 
     checkUnderTen = (time) => { return Number(time) < 10 ? '0'+time : time }
 
-    Timesetting = (checked) => {
-        if (checked) {
-            if (checked.includes('시작')) {
-                this.setState({ time: 2 });
-            } else if (checked.includes('끝')) {
-                this.setState({ time: 3 });
-            } else {
-                this.setState({ time: 1 });
-            }
-        } else {
-            this.setState({ time: 0 });
-        }
-    }
-
     componentDidMount() {
         this.timerID = setInterval(
             () => {
@@ -36,8 +22,10 @@ class Timer extends React.Component {
                 const min = String(this.checkUnderTen(this.state.date.getMinutes()))
                 if (min != this.state.minute) {
                     const checked = Timechecker(hour + min);
-                    this.Timesetting(checked);
-                    this.setState({ period: checked });
+                    if (checked != null) {
+                        this.setState({ time: checked[0] })
+                        this.setState({ period: checked[1] });
+                    }
                 }
                 this.setState({ minute: this.state.date.getMinutes()});
             },
@@ -46,13 +34,13 @@ class Timer extends React.Component {
     }
 
     render() {
-        const table = ['일과 외', '자습시간', '수업시간', '쉬는시간', '식사'];
-        const period = (this.state.period == null) ? null : <div>{String(this.state.period)}</div>
+        const table = ['일과 외', '수업시간', '쉬는시간', '식사', '청소시간', '자습시간'];
+        const period = (this.state.period == null) ? null : <div>{String(table[this.state.period])}</div>
         return (
             <div>
                 <Clock format={'HH:mm:ss'} ticking={true}/>
                 {period}
-                <div>{table[this.state.time]}</div>
+                <div>{this.state.time}</div>
             </div>
         )
     }
